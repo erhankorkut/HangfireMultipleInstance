7c556100716c891ead1db9d49fb8b36f14d9641c
Erhan Korkut <erhankorkut@hotmail.com.tr>
2017-05-25 23:58:01 +0300

    log4net setup

---
+++ HangfireMultipleInstance/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+[assembly: XmlConfigurator]
+namespace HangfireMultipleInstance
+{
+    public class Global : System.Web.HttpApplication
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Global));
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Logger.Info("Application started");
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            Logger.Info("Application shutting down");
+        }
+    }
+}
